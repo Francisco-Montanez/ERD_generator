17cd111b29917c8c80a2bc9cab2bba3c08ef8aec
Francisco-Montanez <32565971+Francisco-Montanez@users.noreply.github.com>
2023-09-07 21:30:01 -0500

    Add a column to the tables showing pk or fk, basic logging, save location option

--- erd_generator/core.py
+++ erd_generator/core.py
@@ -1,6 +1,9 @@
+import os, logging
 from sqlalchemy import create_engine, MetaData, Table
 from graphviz import Digraph
 
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def load_metadata(database_url, schema_name, tables_to_include):
     """
@@ -15,10 +18,9 @@ def load_metadata(database_url, schema_name, tables_to_include):
     metadata = MetaData(schema=schema_name)
 
     with engine.connect() as connection:
-        if tables_to_include:
-            [Table(name, metadata, autoload_with=connection, schema=schema_name) for name in tables_to_include]
-        else:
-            metadata.reflect(bind=connection, schema=schema_name)
+        tables = tables_to_include or metadata.reflect(bind=connection, schema=schema_name).tables.keys()
+
+        [Table(name, metadata, autoload_with=connection, schema=schema_name) for name in tables]
 
     return metadata
 
@@ -29,7 +31,9 @@ def render_column(column):
     :param column: SQLAlchemy Column object.
     :return: HTML string for the column.
     """
-    return f'<tr><td border="1" cellpadding="4">{column.name}</td><td border="1" cellpadding="4">{column.type}</td></tr>'
+    column_type = 'PK' if column.primary_key else 'FK' if column.foreign_keys else ''
+
+    return f'<tr><td>{column.name}</td><td>{column.type}</td><td>{column_type}</td></tr>'
 
 def render_relationship(column, processed_relationships, erd):
     """
@@ -40,18 +44,14 @@ def render_relationship(column, processed_relationships, erd):
     :param erd: Graphviz object representing the ERD.
     """
     for fk in column.foreign_keys:
-        referenced_table = fk.column.table.name
-        
-        relationship = (column.table.name, referenced_table, column.name, fk.column.name)
+        relationship = (column.table.name, fk.column.table.name, column.name, fk.column.name)
 
         if relationship not in processed_relationships:
             processed_relationships.add(relationship)
-            
-            edge_label = f"{column.name} ⟶ {fk.column.name}"
 
-            erd.node(edge_label, label=edge_label, shape='plaintext', fontsize='10', fontcolor='blue', width='0', height='0')
-            erd.edge(column.table.name, edge_label, arrowhead='none', constraint='true')
-            erd.edge(edge_label, referenced_table, constraint='true')
+            edge_label = f"[{relationship[0]}.{relationship[2]}] references [{relationship[1]}.{relationship[3]}]"
+
+            erd.edge(relationship[0], relationship[1], label=edge_label, fontsize='10', fontcolor='blue')
 
 def render_table(table, erd):
     """
@@ -60,34 +60,38 @@ def render_table(table, erd):
     :param table: SQLAlchemy Table object.
     :param erd: Graphviz object representing the ERD.
     """
-    rows = [render_column(column) for column in table.columns]
+    rows = ''.join(map(render_column, table.columns))
     
-    table_html = f'<<table border="1" cellspacing="0" cellpadding="4"><tr><td bgcolor="lightblue" colspan="2" border="1">{table.name}</td></tr>' + ''.join(rows) + '</table>>'
-    
+    table_html = f'<<table><tr><td bgcolor="lightblue" colspan="3">{table.name}</td></tr>{rows}</table>>'
+
     erd.node(table.name, label=table_html, shape='plaintext')
 
-def generate_erd(database_url, schema_name, tables_to_include):
+def generate_erd(database_url, schema_name, tables_to_include, output_directory, cleanup):
     """
     Generate an Entity-Relationship Diagram (ERD) for the specified database.
 
     :param database_url: Connection URL for the database.
     :param schema_name: Specific schema name within the database.
     :param tables_to_include: List of table names to include in the ERD; includes all if None.
+    :param output_directory: Directory to save the ERD.
+    :param cleanup: Whether to clean up the generated dot file.
     """
     try:
         metadata = load_metadata(database_url, schema_name, tables_to_include)
         
-        erd = Digraph(name='ERD', filename='ERD', format='png', node_attr={'margin': '0'})
+        erd = Digraph(name='ERD', filename=f'{output_directory}/ERD', format='png', node_attr={'margin': '0'})
 
         processed_relationships = set()
 
         for table in metadata.sorted_tables:
             render_table(table, erd)
-            
-            for column in table.columns:
-                render_relationship(column, processed_relationships, erd)
+        
+            list(map(lambda column: render_relationship(column, processed_relationships, erd), table.columns))
 
-        erd.view()
+        if not os.path.exists(output_directory):
+            os.makedirs(output_directory)
+
+        erd.render(f"{output_directory}/ERD", cleanup=cleanup)
 
     except Exception as e:
-        print(f"Error generating ERD: {e}")
+        logging.error(f"Error generating ERD: {e}")
